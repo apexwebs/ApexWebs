f842ef8d4695a42cb2a7b9c37f4fcf018883a7e3
Your GitHub Username <ochandaben@gmail.com>
2025-07-03 11:37:46 +0300

    Still on page.tsx

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function AboutPage() {
+  // eslint-disable-next-line react/no-unescaped-entities
   return (
     <section
       style={{
